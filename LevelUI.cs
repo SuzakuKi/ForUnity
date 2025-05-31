347f789d171a7140c6b56287d324d4bcd86e08a9
SuzakuKi <vladghoul@mail.ru>
2025-05-31 10:55:25 +0300

    Create LevelUI.cs

---
+++ LevelUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class LevelUI : MonoBehaviour
+{
+    public Text levelText;
+ 
+    private void Update()
+    {
+        levelText.text = LevelController.level.ToString();
+    }
