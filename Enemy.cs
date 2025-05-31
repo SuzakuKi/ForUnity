d4bca8114b810d35b75d208c37ea68c123549611
SuzakuKi <vladghoul@mail.ru>
2025-05-31 12:43:27 +0300

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,7 +1,8 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class Enemy : MonoBehaviour
 {
     public int health;
@@ -10,35 +11,37 @@ public class Enemy : MonoBehaviour
     public Animator animator;
     public float shootInterval = 1f;
     public float shootTimer = 0f;
- 
+
     public float speedPerLevel = 0.2f;
- 
+
+    public int price;
+
     private void Start()
     {
         speed += speedPerLevel * LevelController.level;
- 
+
         borderPosX = Corn.singleton.transform.position.x;
     }
- 
+
     private void Update()
     {
         if (shootTimer > 0)
         {
             shootTimer -= Time.deltaTime;
         }
- 
+
         float enemyPosX = transform.position.x;
- 
+
         if (enemyPosX > borderPosX)
         {
             animator.SetBool("isMoving", true);
             transform.position += -transform.right * speed * Time.deltaTime;
- 
+
         }
         else
         {
             animator.SetBool("isMoving", false);
- 
+
             if (shootTimer <= 0)
             {
                 Attack();
@@ -46,15 +49,21 @@ public class Enemy : MonoBehaviour
             }
         }
     }
- 
+
     public void TakeDamage()
     {
         health -= 1;
- 
+
         if (health <= 0)
-            Destroy(gameObject);
+            Die();
     }
- 
+
+    public void Die()
+    {
+        Corn.singleton.AddCrystals(price);
+        Destroy(gameObject);
+    }
+
     public void Attack()
     {
         Corn.singleton.TakeDamage();
